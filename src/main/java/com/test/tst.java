d9020a34defde1fd68d778b84e53d043f98f9be6
master <fscfernando@hotmail.com>
2017-03-03 13:40:34 +0100

    test-v1.0

--- src/main/java/com/test/tst.java
+++ src/main/java/com/test/tst.java
@@ -5,6 +5,7 @@ public class tst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Main.main()" + 10);
+		System.out.println("test-v1.0" + 11);
 	}
 
 }
