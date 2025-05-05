f526e6a4d8cb2964a84fdd438ba93bf2336bdb06
fscfer <fscfernando@hotmail>
2025-05-05 14:06:33 +0200

    update for branch upd-003

--- src/main/java/com/test/tst.java
+++ src/main/java/com/test/tst.java
@@ -5,7 +5,7 @@ public class tst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Main.main()" + 26);
-		System.out.println("test-v1.0" + 10);
+		System.out.println("test-v3.0" + 10);
 	}
 
 }
